05e9f3a1eb73b625083aff9b9d6bd5e3eb328eda
Ada Chavez <ada.r.chavez@gmail.com>
2020-10-14 17:23:25 -0700

    create inheritance.py

---
+++ TTA_Python/Assignments/inheritance.py
@@ -0,0 +1,26 @@
+##
+## Creating two classes that inherit from another class
+##
+##
+
+class home_furnishings:
+    item_name = 'No name provided'
+    sku_number = 9087658
+    aisle = 39
+    price = 400
+
+
+class bedroom(home_furnishings):
+    comforter_filing = 'synthetic down'
+    bedframe_height = '24 inches'
+
+class kitchen(home_furnishings):
+    counter_material = 'marble'
+    sink_material = 'stainless steel'
+    
+    
+    
+    
+    
+    
+    
