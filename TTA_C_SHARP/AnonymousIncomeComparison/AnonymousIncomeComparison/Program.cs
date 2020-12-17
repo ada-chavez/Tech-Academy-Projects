849a79302728909995cda688596d0f15cd5728ec
Ada Chavez <ada.r.chavez@gmail.com>
2020-12-17 09:47:36 -0800

    Initial commit

---
+++ TTA_C_SHARP/AnonymousIncomeComparison/AnonymousIncomeComparison/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AnonymousIncomeComparison
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anonymous Income Comparison Program\nPerson 1");
+
+            // Input for Person 1
+            Console.WriteLine("Hourly Rate?");
+            double rateOne = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            double weekOne = Convert.ToDouble(Console.ReadLine());
+
+            // Input for Person 2
+            Console.WriteLine("Person 2 \nHourly Rate?");
+            double rateTwo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            double weekTwo = Convert.ToDouble(Console.ReadLine());
+            
+            // Calculates annual salary 
+            double salaryOne = 52 * (rateOne * weekOne);
+            double salaryTwo = 52 * (rateTwo * weekTwo);
+
+            Console.WriteLine("Annual salary of Person 1:\n" + salaryOne);
+            Console.WriteLine("Annual salary of Person 2:\n" + salaryTwo);
+
+            // Is annual salary of Person 1 greater than Person 2?
+            bool makeMore = salaryOne > salaryTwo;
+            Console.WriteLine("Does Person 1 make more money than Person 2?\n" + makeMore);
+
+            Console.ReadLine();
+        }
+    }
+}
